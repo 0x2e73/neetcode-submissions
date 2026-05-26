eb0db00b78aed32897df8270fc30d0fc63e0161c
.s <89457451+0x2e73@users.noreply.github.com>
2026-05-26 15:03:39 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cs
@@ -0,0 +1,39 @@
+public class Solution {
+ public bool IsValidSudoku(char[][] board) {
+        HashSet<char>[] rows = new HashSet<char>[9];
+        HashSet<char>[] cols = new HashSet<char>[9];
+        HashSet<char>[] boxes = new HashSet<char>[9];
+        
+        //nul ref attention
+        for (int k = 0; k < 9; k++)
+        {
+            rows[k] = [];
+            cols[k] = [];
+            boxes[k] = [];
+        }
+
+        //parcours les lignes
+        for(int i = 0; i < 9; i++)
+        {
+            //parcours les col
+            for(int j = 0; j < 9; j++)
+            {
+                if(board[i][j] != '.')
+                {
+                    var valueChar = Convert.ToChar(board[i][j]);
+                    if(
+                        rows[i].Contains(valueChar) || 
+                        cols[j].Contains(valueChar) || 
+                        boxes[(i / 3) * 3 + (j/ 3)].Contains(valueChar)
+                        )
+                        return false;
+
+                    rows[i].Add(valueChar);
+                    cols[j].Add(valueChar);
+                    boxes[(i / 3) * 3 + (j / 3)].Add(valueChar);
+                }
+            }
+        }
+        return true;
+    }
+}
