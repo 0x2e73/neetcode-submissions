2e2e09a1225d96fba05b7456a22ad5bfabc7f108
.s <89457451+0x2e73@users.noreply.github.com>
2026-08-09 21:20:39 +0200

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if(nums.Length == 0) return 0;
+        var hash = new HashSet<int>(nums);
+
+        int count = 1;
+        int maxCount = 1;
+
+        foreach (var item in hash)
+        {
+            if (!hash.Contains(item - 1))
+            {
+                int current = item;
+                while (hash.Contains(current + 1))
+                {
+                    count++;
+                    current++;
+                }
+                
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+                count = 1;
+            }
+        }
+
+        return maxCount;
+    }
+}
